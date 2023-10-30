76ee6cdc2666b5600a7aff1a707f6c54ce2abf8b
Zero <zero.js.dev@gmail.com>
2023-10-30 09:39:09 +0330

    Hide debug info

--- src/helpers/platform.ts
+++ src/helpers/platform.ts
@@ -1,5 +1,5 @@
 export const isMobile = () => {
-    alert(navigator.userAgent)
+    // alert(navigator.userAgent)
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
     .test(navigator.userAgent) //|| window.matchMedia("only screen and (max-width: 760px)").matches
 }
